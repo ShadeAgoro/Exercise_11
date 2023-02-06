68d3975cc1a5407f2d31201d4791e50475cb79ff
Shade Agoro <Shadeagoro@hotmail.co.uk>
2023-02-06 19:08:27 +0000

    added exercise 11 file in right place

--- ex11_starter.py
+++ ex11_starter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
-# Exercise 11a
+
+#  *Exercise 11A*
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 print(len(Belgium)) # printing the length/ number of characters in Belgium string variable
 print(Belgium) # printing string alongside hyphens
 print("-"*81) # running command in terminal to multiply hyphens by the length/ number of characters in string variable
 
-print("-"*len(Belgium)) # tidied/ simplified version to multiply hyphens by length/ number of string characters by it doesn't show string length = 81
+print("-"*len(Belgium)) # tidied simplified version to multiply hyphens by length/ number of string characters by it doesn't show string length = 81
 print(Belgium)
 
-# Exercise 11b
+#  *Exercise 11B*
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 Belgium_colon = Belgium.replace(',',':')
 # created a new variable to display a new line of string for variable 'Belgium' with commas
 # use of the replace method to change comma separators into colon
 print(Belgium_colon)
 
-# Exercise 11c
+#  *Exercise 11C*
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 print(Belgium.split(','))
 
